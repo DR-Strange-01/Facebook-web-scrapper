2be506a2b16c8ab0f6c84a2e52753ab41ec5ef9a
Dr-Strange-01 <125406268+DR-Strange-01@users.noreply.github.com>
2024-09-06 22:05:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from facebook_page_info_scraper import FacebookPageInfoScraper
-import uvicorn
 
 app = FastAPI()
 
@@ -14,7 +13,3 @@ def get_followers(username: str):
     page_followers = page_info.get('page_followers', 'Data not available')
 
     return {"page_followers": page_followers}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
