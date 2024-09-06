69e20c1deaac2a6f58852fc321720ac927795d8f
Dr-Strange-01 <125406268+DR-Strange-01@users.noreply.github.com>
2024-09-06 21:32:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import os
 from fastapi import FastAPI
 from facebook_page_info_scraper import FacebookPageInfoScraper
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/followers")
 def get_followers(username: str):
-    # Construct the page URL
     page_url = f'https://www.facebook.com/{username}'
 
-    # Create an instance of the scraper
     scraper = FacebookPageInfoScraper(link=page_url)
     page_info = scraper.get_page_info()
 
-    # Extract only the page followers
     page_followers = page_info.get('page_followers', 'Data not available')
 
     return {"page_followers": page_followers}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
