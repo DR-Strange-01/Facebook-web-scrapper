d752afff1a5b2040a685158784c7d18fa013c864
Dr-Strange-01 <125406268+DR-Strange-01@users.noreply.github.com>
2024-09-06 22:05:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 from fastapi import FastAPI
 from facebook_page_info_scraper import FacebookPageInfoScraper
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import uvicorn
 
 app = FastAPI()
 
-def setup_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
-
-    service = Service(executable_path=os.getenv("CHROMEDRIVER_PATH", ChromeDriverManager().install()))
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
-
-@app.get("/followers/{username}")
+@app.get("/followers")
 def get_followers(username: str):
-    page_url = f"https://www.facebook.com/{username}"
-    
-    scraper = FacebookPageInfoScraper(link=page_url, custom_driver_func=setup_driver)
-    followers = scraper.get_followers()
-    return {"followers": followers}
+    page_url = f'https://www.facebook.com/{username}'
+
+    scraper = FacebookPageInfoScraper(link=page_url)
+    page_info = scraper.get_page_info()
+
+    page_followers = page_info.get('page_followers', 'Data not available')
+
+    return {"page_followers": page_followers}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
